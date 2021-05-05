5cc44632dd4a57b2f91c302c4d78e232725c63c9
Mark Swan <ms.freemind@gmail.com>
2021-05-04 18:34:13 -0600

    Committing files

---
+++ display.rb
@@ -0,0 +1,45 @@
+require "colorize"
+require_relative "cursor"
+require_relative "board"
+
+class Display
+  attr_reader :cursor
+
+  def initialize (board)
+    @board = board
+    @cursor = Cursor.new([0,0], board)
+  end
+
+  def render
+    system("clear")
+    print "\n  0 1 2 3 4 5 6 7"
+
+    (0..7).each do |row|
+      print "\n#{row}"
+      (0..7).each do |col|
+        render_pos(row, col)
+      end
+    end
+
+    print "\nCHECK" if @board.in_check?(:w) || @board.in_check?(:b)
+    print "\n"
+  end
+
+  private
+
+  def render_pos(row, col)
+    piece = @board[[row, col]]
+    symbol = piece.to_s
+
+    print " "
+    if [row, col] == @cursor.cursor_pos
+      if @cursor.selected
+        print "#{symbol}".colorize(:background => :red)
+      else
+        print "#{symbol}".colorize(:background => :blue)
+      end
+    else        
+      print "#{symbol}"
+    end
+  end
+end
